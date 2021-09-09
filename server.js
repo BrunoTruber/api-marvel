b2d402fa01b33277d793a7ca4f462558c7ed88ff
BrunoTruber <brunotruber45@gmail.com>
2021-09-08 21:38:51 -0300

    new

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectToDb = require("./src/database/database");
 
-connectToDb();
+connectToDb();     vjvhj
 
 const app = express();
 const port = 3000;
